92ee1fe0723ddf9a8111213429abf7f0dc87d8a5
Annie Huang <annie.hn@gmail.com>
2019-12-04 12:40:48 +1100

    Add Sorting01-Solution-MaximumToysPurchases.js

---
+++ Sorting01-Solution-MaximumToysPurchases.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+////////////////////////////////////////////////////////////////////
+// Complete the maximumToys function below.
+function maximumToys(prices, k) {
+    console.log('prices=', prices);
+    console.log('k=', k);
+
+    let sortedPrices = prices.sort((a, b) => {
+        return a - b;
+    });
+    console.log('sortedPrices=', sortedPrices);
+
+    let totalNumberOfToys = 0;
+    let accAmount = 0;
+    let i = 0;
+
+    while (i<sortedPrices.length) {
+        if(accAmount + sortedPrices[i] <= k) {
+            accAmount += sortedPrices[i];
+            i++;
+        } else {
+            break;
+        }
+    }
+
+    return i;
+
+}
+////////////////////////////////////////////////////////////////////
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const prices = readLine().split(' ').map(pricesTemp => parseInt(pricesTemp, 10));
+
+    let result = maximumToys(prices, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
